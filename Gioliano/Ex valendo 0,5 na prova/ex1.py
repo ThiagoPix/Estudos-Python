e23519a592df034cc7d7c20506cccd2ef1d7c8f8
ThiagoPix <126736734+ThiagoPix@users.noreply.github.com>
2023-04-18 00:25:11 -0300

    att

---
+++ Gioliano/Ex valendo 0,5 na prova/ex1.py
@@ -0,0 +1,30 @@
+totalAbono = 0
+funcionariototal = 0
+maiorAbono = 0
+minimoAbono = 0
+
+
+while True:
+    salario = float (input("Digite o salario. (Aperte 0 e de enter para cancelar!)"))
+    if salario == 0: 
+        break
+    else:   
+        abono = salario * 0.2
+        if abono < 100 and abono >0:
+            abono = 100
+
+        totalAbono = totalAbono + abono
+        if abono == 100:
+            minimoAbono += 1
+        
+        if abono > maiorAbono:
+            maiorAbono = abono
+
+        print ('Salario - Abono ')
+        print("R$", salario, "- R$",abono )
+
+print("\nProjeção de Gastos com Abono")
+print("============================")
+print("Total gasto com abono: R$", totalAbono)
+print("Valor mínimo pago a", minimoAbono, "colaboradores" )
+print("Maior valor de abono pago: R$", maiorAbono)
